c331c418ba7b73bcf377f86f744de9e49d07f8d3
Mizanur Rahman <mizan@arogga.net>
2023-08-01 11:29:17 +0600

    visible progress bar

--- App.tsx
+++ App.tsx
@@ -1,50 +1,70 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
-import ProgressBar from './components/ProgressBar';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+import SignUpProgress from './components/SignUpProgress';
 
 const App = () => {
-  const [activeStepIndex, setActiveStepIndex] = useState(0);
-
-  const steps = ['Personal Info', 'Account Setup', 'Verification', 'Confirmation'];
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [activeStep, setActiveStep] = useState(0);
 
   const handleNextStep = () => {
-    setActiveStepIndex((prevIndex) => Math.min(prevIndex + 1, steps.length - 1));
+    setActiveStep((prevStep) => prevStep + 1);
   };
 
   const handlePreviousStep = () => {
-    setActiveStepIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+    setActiveStep((prevStep) => prevStep - 1);
   };
+
+  const handleSignUp = () => {
+    // Implement your sign-up logic here
+    console.log('Signing up with email:', email, 'and password:', password);
+  };
+
+  const steps = ['Email', 'Password', 'Confirm Password'];
 
   return (
     <View style={styles.container}>
-      <ProgressBar steps={steps} activeIndex={activeStepIndex} />
-      <View style={styles.stepContentContainer}>
-        {activeStepIndex === 0 && (
-          <View>
-            {/* Render your Personal Info form here */}
-            <Button title="Next" onPress={handleNextStep} />
-          </View>
-        )}
-        {activeStepIndex === 1 && (
-          <View>
-            {/* Render your Account Setup form here */}
-            <Button title="Previous" onPress={handlePreviousStep} />
-            <Button title="Next" onPress={handleNextStep} />
-          </View>
-        )}
-        {activeStepIndex === 2 && (
-          <View>
-            {/* Render your Verification form here */}
-            <Button title="Previous" onPress={handlePreviousStep} />
-            <Button title="Next" onPress={handleNextStep} />
-          </View>
-        )}
-        {activeStepIndex === 3 && (
-          <View>
-            {/* Render your Confirmation page here */}
-            <Button title="Previous" onPress={handlePreviousStep} />
-            <Button title="Finish" onPress={() => console.log('Sign-up completed!')} />
-          </View>
+      <SignUpProgress steps={steps} activeStep={activeStep} />
+
+      {/* Render form fields based on activeStep */}
+      {activeStep === 0 && (
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={setEmail}
+          value={email}
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+      )}
+
+      {activeStep === 1 && (
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry
+        />
+      )}
+
+      {activeStep === 2 && (
+        <TextInput
+          style={styles.input}
+          placeholder="Confirm Password"
+          onChangeText={setConfirmPassword}
+          value={confirmPassword}
+          secureTextEntry
+        />
+      )}
+
+      <View style={styles.buttonContainer}>
+        {activeStep > 0 && <Button title="Previous" onPress={handlePreviousStep} />}
+        {activeStep < steps.length - 1 ? (
+          <Button title="Next" onPress={handleNextStep} />
+        ) : (
+          <Button title="Sign Up" onPress={handleSignUp} />
         )}
       </View>
     </View>
@@ -54,12 +74,19 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    padding: 20,
   },
-  stepContentContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  input: {
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
   },
 });
 
